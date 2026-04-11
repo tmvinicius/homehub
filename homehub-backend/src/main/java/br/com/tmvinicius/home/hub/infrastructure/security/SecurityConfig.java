eebfcfbf5f8c19d93cb1749d79706fc4a19c1ec2
TMVinicius <viniciustavaresbr@gmail.com>
2026-04-11 19:48:18 -0300

    add loaded to bean inicialization in SecurityConfig

--- homehub-backend/src/main/java/br/com/tmvinicius/home/hub/infrastructure/security/SecurityConfig.java
+++ homehub-backend/src/main/java/br/com/tmvinicius/home/hub/infrastructure/security/SecurityConfig.java
@@ -6,11 +6,13 @@ import br.com.tmvinicius.home.hub.infrastructure.security.jwt.JwtProperties;
 import br.com.tmvinicius.home.hub.infrastructure.security.jwt.JwtTokenAdapter;
 import br.com.tmvinicius.home.hub.infrastructure.security.password.BCryptPasswordAdapter;
 import br.com.tmvinicius.home.hub.infrastructure.security.password.BCryptProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableConfigurationProperties({JwtProperties.class, BCryptProperties.class})
 public class SecurityConfig {
 
     @Bean
