f323ef04f0d79940543083cfda888e6e88a553f1
tmvinicius <viniciustavaresbr@gmail.com>
2026-08-02 16:04:14 -0300

    feat(auth): implement access token refresh flow

--- homehub-backend/src/main/java/br/com/tmvinicius/home/hub/domain/port/out/auth/RefreshTokenRepository.java
+++ homehub-backend/src/main/java/br/com/tmvinicius/home/hub/domain/port/out/auth/RefreshTokenRepository.java
@@ -2,7 +2,11 @@ package br.com.tmvinicius.home.hub.domain.port.out.auth;
 
 import br.com.tmvinicius.home.hub.domain.model.auth.RefreshToken;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository {
 
     RefreshToken save(RefreshToken refreshToken);
+
+    Optional<RefreshToken> findByToken(String token);
 }
