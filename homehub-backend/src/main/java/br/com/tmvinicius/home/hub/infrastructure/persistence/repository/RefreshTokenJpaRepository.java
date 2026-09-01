4d90cc58b0c6f9082299346aa789bfcd988731f8
tmvinicius <viniciustavaresbr@gmail.com>
2026-08-31 22:05:18 -0300

    refactor: change repositories from CrudRepository to JpaRepository

--- homehub-backend/src/main/java/br/com/tmvinicius/home/hub/infrastructure/persistence/repository/RefreshTokenJpaRepository.java
+++ homehub-backend/src/main/java/br/com/tmvinicius/home/hub/infrastructure/persistence/repository/RefreshTokenJpaRepository.java
@@ -1,12 +1,12 @@
 package br.com.tmvinicius.home.hub.infrastructure.persistence.repository;
 
 import br.com.tmvinicius.home.hub.infrastructure.persistence.auth.RefreshTokenPersistence;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface RefreshTokenJpaRepository extends CrudRepository<RefreshTokenPersistence, UUID> {
+public interface RefreshTokenJpaRepository extends JpaRepository<RefreshTokenPersistence, UUID> {
 
     Optional<RefreshTokenPersistence> findByToken(String token);
 }
